f8e9157191b96a5f7389b142d10a988713d82577
mahdi <mahdi.ramezani.059@gmail.com>
2024-07-25 19:10:25 +0330

    heve a problem in admin

--- blog/views.py
+++ blog/views.py
@@ -15,9 +15,10 @@ def post_list(request):
 
 def post_detail(request , post_id):
     post = Post.objects.get(id=post_id)
-
+    coments = Coments.objects.get(id=post_id)
     context = {
-        "post" : post
+        "post" : post,
+        "coments" : coments,
     }
 
     return render(request , "post/deatil.html" , context)
