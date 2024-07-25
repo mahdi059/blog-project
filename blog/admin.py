8c5b72d75ad60e98cc363c79c94895435fe308e8
mahdi <mahdi.ramezani.059@gmail.com>
2024-07-25 16:29:16 +0330

    create all models and regester in admin and migrate

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Post , Categories , Coments , Likes , add_favorite 
 
-# Register your models here.
+admin.site.register(add_favorite)
+admin.site.register(Likes)
+admin.site.register(Coments)
+admin.site.register(Categories)
+admin.site.register(Post)
+
--- blog/models.py
+++ blog/models.py
@@ -1,4 +1,62 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.core.exceptions import ValidationError
 
-# Create your models here.
+def validate_rating(value):
+    if value < 1 or value > 5 :
+        raise ValidationError('rating moust be between 1 and 5 ')
+
+class Categories(models.Model):
+    category = models.CharField(max_length=100)
+    bio = models.TextField()
+    parents = models.ForeignKey('self' ,on_delete=models.CASCADE ,related_name='children' , blank=True , null=True)
+
+    def __str__(self):
+        return self.category
+
+class Post(models.Model):
+    DRAFT = 'draft'
+    PUBLISHED = 'published'
+    
+    STATUS_CHOICES = [
+        (DRAFT, 'Draft'),
+        (PUBLISHED, 'Published'),
+    ]
+    title = models.CharField(max_length=50)
+    img = models.ImageField(upload_to='media/post')
+    body = models.TextField()
+    creat_at = models.DateTimeField(auto_now_add= True)
+    update_at = models.DateTimeField(auto_now=True)
+    status = models.CharField(
+        max_length=10,
+        choices=STATUS_CHOICES,
+        default=DRAFT,
+    )
+    author = models.ForeignKey(User , on_delete= models.CASCADE)
+    category = models.ForeignKey(Categories , related_name='posts' , on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
+
+class Coments(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE )
+    coment_authors = models.ForeignKey(User , on_delete= models.CASCADE)
+    body = models.TextField()
+    create_at = models.DateTimeField(auto_now_add= True)
+    parents = models.ForeignKey('self' ,on_delete=models.CASCADE ,related_name='children' , blank=True , null=True)
+    rating = models.PositiveSmallIntegerField(validators=[validate_rating])
+
+    def __str__(self):
+        return f"{self.post.title} - coments"
+
+class Likes(models.Model):
+    post = models.ForeignKey(Post , on_delete= models.CASCADE)
+    user = models.ForeignKey(User , on_delete=models.CASCADE)
+
+class add_favorite(models.Model):
+    post = models.ForeignKey(Post , on_delete= models.CASCADE)
+    user = models.ForeignKey(User , on_delete=models.CASCADE)
+
+
+
 
