0c51c67145dab4f99b8ff3e9c6e2638e03e72893
mahdi <mahdi.ramezani.059@gmail.com>
2024-07-25 18:57:26 +0330

    add views for list and deatil and add template for theme

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,23 @@
 from django.shortcuts import render
+from .models import Post , Categories , Coments , Likes , add_favorite
 
-# Create your views here.
+
+
+def post_list(request):
+
+    post = Post.objects.filter(status = 'published')
+
+    context = {
+        "post" : post
+        }
+
+    return render(request , 'post/list.html' , context)
+
+def post_detail(request , post_id):
+    post = Post.objects.get(id=post_id)
+
+    context = {
+        "post" : post
+    }
+
+    return render(request , "post/deatil.html" , context)
